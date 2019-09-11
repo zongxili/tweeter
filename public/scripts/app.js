cb49e4268a543931aaecac968cc062edc2071064
Zongxi Li <zongxili@Zongxis-MacBook-Pro.local>
2019-09-11 10:09:15 -0700

    something changed

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,37 +4,69 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
- const createTweetElement = function(dataObject) {
-    const userName = dataObject.user.name;
-    const userHandle = dataObject.user.handle;
-    const userContent = dataObject.content.text;
-    const createdTime = dataObject.created_at;
-
-    let newArticle = document.createElement("article.tweet");
-    var element = document.getElementById("#userName");
-    element.nodeValue() = 
- };
-
-
-// Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
       "handle": "@SirIsaac"
     },
-  "content": {
+    "content": {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
-  "created_at": 1461116232227
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense, donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+const createTweetElement = function(dataObject) {
+  const userName = dataObject.user.name;
+  const userHandle = dataObject.user.handle;
+  const userContent = dataObject.content.text;
+  const createdTime = dataObject.created_at;
+
+  const markup = `
+    <article class="tweet">
+      <header>
+        <div class="left"> 
+          <span id="userName">${userName}</span>
+        </div>
+        <div class="right"> 
+          <span id="userHandle">${userHandle}</span>
+        </div>
+      </header>
+
+      <p class="tweetText">${userContent}</p>
+
+      <footer>
+        <div class="date">
+          <span id="timeCreated">${createdTime}</span>
+        </div>
+
+        <div class="right"> 
+          <span>Some icon here</span>
+        </div>
+      </footer>
+    </article>`;
+  return $(markup);
+};
+
+const renderTweets = function(tweets) {
+  for (let i = 0; i < tweets.length; i ++) {
+    $('#tweets-container').append(createTweetElement(tweets[i])); 
+  }
 }
 
-const $tweet = createTweetElement(tweetData);
-
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-// $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-
-function geeks() { 
-  var node = document.createElement("LI");
-} 
+$("form").submit(function( event ) {
+  renderTweets(data);
+  // $(#textArea)
+});
