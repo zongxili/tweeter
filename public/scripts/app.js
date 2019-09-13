5256682c2d22379bcaae8dc005395093d273d6b7
Zongxi Li <zongxili@Zongxis-MBP.hitronhub.home>
2019-09-13 07:35:24 -0700

    Final version

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,31 +3,6 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense, donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
 const createTweetElement = function(dataObject) {
   const userName = dataObject.user.name;
   const userHandle = dataObject.user.handle;
@@ -62,11 +37,85 @@ const createTweetElement = function(dataObject) {
 
 const renderTweets = function(tweets) {
   for (let i = 0; i < tweets.length; i ++) {
-    $('#tweets-container').append(createTweetElement(tweets[i])); 
+    $('#tweets-container').prepend(createTweetElement(tweets[i])); 
   }
 }
 
-$("form").submit(function( event ) {
-  renderTweets(data);
-  // $(#textArea)
+// $("form").submit(function( event ) {
+//   renderTweets(data);
+//   // $(#textArea)
+// });
+
+// Ajax part
+// const $submitButton = $('#tweet-submit-button');
+// $submitButton.on('click', function (event) {
+//   // setTimeout(function(){ 
+//   //   console.log('Button clicked, performing ajax call...');
+//   //   alert(event);
+//   // }, 1000);
+//   console.log('Button clicked, performing ajax call...');
+//   $.ajax('../index.html', { method: "POST" })
+//   .then(function (morePostsHtml) {
+//     console.log('Success: ', morePostsHtml);
+//     alert("THis is "+ morePostsHtml);
+//   });
+// });
+
+const escape =  function(str) {
+  let div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
+
+$(document).ready(function() { // Runs reloading the page
+
+  $("#arrow-down-button").click(function(){
+    $(".new-tweet").animate({
+      height: 'toggle'
+    });
+  });
+
+  const loadTweets = function() {
+    $('#tweetsArea').val("");
+    $.ajax('/tweets', { method: 'GET' })
+    .then(function (response) {
+      renderTweets(response);
+    });
+  };
+  loadTweets();
+
+  // This function is needed to be here since it uses the function loadTweets
+  $("#tweet-form").on( "submit", function( event ) { // Submiting a tweets
+    event.preventDefault();
+    console.log('ABOUT TO SUBMIT FORM!!!!');
+    let inputValue = $('#tweetsArea').val();
+    if (inputValue === "" || inputValue === null) {
+      $("#error-msg").text("The input is empty or null, please go check it");
+      $(".error-msg-section").animate({
+        height: 'toggle'
+      });
+    } else if (inputValue.length > 140) { // this approximately won't happen, since "max-length" added to the TextArea tag
+      $("#error-msg").text("The length of input is above the Word Limitation.");
+      $(".error-msg-section").animate({
+        height: 'toggle'
+      });
+      // $('.error-msg-section').addClass('visible');
+    }
+    else {
+      inputValue = `<p>${escape(inputValue)}</p>`;
+      console.log(event);
+      event.preventDefault();
+      $.ajax({
+        url: '/tweets',
+        type: 'POST',
+        data: $(this).serialize()
+      })
+        .then(() => {
+          $('#tweets-container').empty();
+          loadTweets(); // Update dat list
+        });
+    }
+  });
+  // return false;
 });
+
